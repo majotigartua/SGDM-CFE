04ca62e777ed0db402d823fa611a4954f6c9bc90
María José Torres <majotigartua@gmail.com>
2024-10-11 12:53:34 -0600

    creation of project files

---
+++ SGDM-CFE.UI/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace SGDM_CFE.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Window loginWindow = new LoginWindow();
+            loginWindow.Activate();
+        }
+    }
+}
---
+++ SGDM-CFE.UI/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace SGDM_CFE.UI
+{
+    public sealed partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
